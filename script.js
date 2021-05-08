334b54a6860348d4d845fc1aca1505063abcbfee
Mahmoud Arobie <mahmoud.ud@gmail.com>
2021-05-08 10:11:31 +0300

    comming soon

---
+++ script.js
@@ -0,0 +1,22 @@
+const countDown =()=>{
+    const countDate= new Date("June 19, 2021 00:00:00").getTime();
+    const now= new Date().getTime();
+    const gap = countDate-now;
+    //how the time does work
+    const second = 1000;
+    const minute = 60*second;
+    const hour = 60*minute;
+    const day = 24*hour;
+    // collect the days
+    const textDay = Math.floor(gap/day);
+    const textHour = Math.floor((gap%day)/hour);
+    const textMinut = Math.floor((gap%hour)/minute);
+    const textSecond = Math.floor((gap%minute)/second);
+    document.querySelector(".day").innerHTML= textDay;
+    document.querySelector(".hour").innerHTML= textHour;
+    document.querySelector(".minute").innerHTML= textMinut;
+    document.querySelector(".second").innerHTML= textSecond;
+   
+}
+
+setInterval(countDown,1000);
